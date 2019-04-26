8c036f30fa6b72a4be831a33e0b0bf2cb180ebab
k15024 <k15024kk@aitech.ac.jp>
2019-04-26 19:01:07 +0900

    fix install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,12 @@
 
 for f in .??*
 do
-    [[ "$f" == ".git" ]] && continue
-    [[ "$f" == ".DS_Store" ]] && continue
+	[[ "$f" == ".git" ]] && continue
+	[[ "$f" == ".gitignore" ]] && continue
+	[[ "$f" == ".DS_Store" ]] && continue
 
 	ln -sf ~/dotfiles/$f ~/
 	
-    echo "Deploy $f complete"
+	echo "Deploy $f complete"
 	
 done
